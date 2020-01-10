09e90f3df618e3f3381e731d18ffa9c79068e209
Mateusz Miloszewski <monitorwebdesign@gmail.com>
2020-01-10 20:30:39 +0100

    Update MapContainer.js

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -12,7 +12,16 @@ class MapContainer extends Component {
           defaultZoom={11} // Supports DP, e.g 11.5
           defaultCenter={{ lat: 1.3521, lng: 103.8198 }}
           yesIWantToUseGoogleMapApiInternals={true}
-          onGoogleApiLoaded={({ map, maps }) => this.apiHasLoaded(map, maps)}
+          onGoogleApiLoaded={({ map, maps }) => this.apiHasLoaded(map, // maps === mapsApi. But I changed it for clearer naming.apiHasLoaded = ((map, mapsApi) => {
+            this.setState({
+              mapsApi,
+              autoCompleteService: new mapsApi.places.AutocompleteService(),
+              placesService: new mapsApi.places.PlacesService(map),
+              directionService: new mapsApi.DirectionsService(),
+              geoCoderService: new mapsApi.Geocoder(),
+              singaporeLatLng: new mapsApi.LatLng(1.3521, 103.8198)
+          });
+          )}
         >
         </GoogleMapReact>
       </section>
